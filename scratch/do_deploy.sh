40e2ba616fdf85c5feafb5856ee03514d739cd18
Dieter Olson <dieterolson@gmail.com>
2026-05-21 17:44:57 -0700

    fix: keep ci checks real while repairing hooks

--- scratch/do_deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
-bash /mnt/c/Users/diete/Repositories/Runner_Dashboard/deploy/update-deployed.sh --repo /mnt/c/Users/diete/Repositories/Runner_Dashboard
